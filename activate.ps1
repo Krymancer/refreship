6959bb20ecaaf59644a0443a725dbf1c1b98d61b
Jose Junior <TR036999@MinhaTI.com.br>
2022-05-10 12:00:08 -0300

    update activate

--- activate.ps1
+++ activate.ps1
@@ -2,20 +2,20 @@ $Folder = "./venv/"
 
 if (-Not (Test-Path -Path $Folder)) { 
   Write-Host "Criando ambiente virtual..."
-  python -m venv venv
+  python -m venv venv | Out-Null
 }
 
 Write-Host "Instalando as dependências..."
-./venv/Scripts/pip install -r requirements.txt
+./venv/Scripts/pip install -r requirements.txt | Out-Null
 
 $File = "./refreship.bat"
 $BinFolder = "./bin/"
 
 if (-Not (Test-Path -Path $BinFolder)) {
-  New-Item -Path "./" -Name "bin" -ItemType "directory"
+  New-Item -Path "./" -Name "bin" -ItemType "directory" | Out-Null
 }
 
 if (-Not (Test-Path -Path $File -PathType leaf)) {
   Write-Host "Criando arquivo de inicialização..."
-  "./venv/Scripts/python ./src/refreship.py" | Out-File -FilePath "./bin/refreship.ps1"
+  "../venv/Scripts/python ../src/refreship.py" | Out-File -FilePath "./bin/refreship.ps1"
 }
