b394e77aa5ab34683ccbbec6eeb6997ccc600cf0
Jose Junior <TR036999@MinhaTI.com.br>
2022-05-10 11:55:15 -0300

    update

--- activate.ps1
+++ activate.ps1
@@ -9,8 +9,13 @@ Write-Host "Instalando as dependências..."
 ./venv/Scripts/pip install -r requirements.txt
 
 $File = "./refreship.bat"
+$BinFolder = "./bin/"
+
+if (-Not (Test-Path -Path $BinFolder)) {
+  New-Item -Path "./" -Name "bin" -ItemType "directory"
+}
 
 if (-Not (Test-Path -Path $File -PathType leaf)) {
   Write-Host "Criando arquivo de inicialização..."
-  "./venv/Scripts/python ./refreship.py" | Out-File -FilePath "./refreship.ps1"
+  "./venv/Scripts/python ./src/refreship.py" | Out-File -FilePath "./bin/refreship.ps1"
 }
